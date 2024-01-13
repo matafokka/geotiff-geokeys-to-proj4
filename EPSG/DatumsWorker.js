a305642eb45a7430bfb4e1b3a78f678d9ed3f7c7
matafokka <da.real.matafokka@gmail.com>
2024-01-13 17:00:51 +0300

    Fixed ensemble datums not being added

--- EPSG/DatumsWorker.js
+++ EPSG/DatumsWorker.js
@@ -5,25 +5,48 @@ const meridians = require("./data/GeogPrimeMeridianGeoKey.js");
 const ellipsoids = require("./data/GeogEllipsoidGeoKey.js");
 const forEach = require("./forEachEntryInEPSG.js");
 
-forEach(`
-	SELECT d.datum_code AS id, d.ellipsoid_code AS e, d.prime_meridian_code AS pm
-	FROM epsg.epsg_datum AS d
-	WHERE (d.ellipsoid_code IS NOT NULL OR d.prime_meridian_code IS NOT NULL)
-	  AND d.datum_type NOT LIKE 'vertical'
-	  AND d.datum_type NOT LIKE 'engineering'
-`, (result) => {
-	let meridian = result.pm.toString();
-	let ellipsoid = result.e.toString();
+const addedIds = {}; // For ensemble datums
 
+forEach(`
+	SELECT
+		d.datum_code AS id,
+		d.ellipsoid_code AS ellipsoid,
+		d.prime_meridian_code AS pm,
+		member_data.ellipsoid_code AS ref_ellipsoid,
+		member_data.prime_meridian_code AS ref_pm
+	FROM epsg.epsg_datum as d
+		LEFT JOIN epsg.epsg_datumensemblemember member ON d.datum_code = member.datum_ensemble_code
+		LEFT JOIN epsg.epsg_datum member_data on member_data.datum_code = member.datum_code
+	WHERE
+		d.datum_type NOT LIKE 'vertical' AND
+		d.datum_type NOT LIKE 'engineering' AND
+		(
+			d.ellipsoid_code IS NOT NULL OR
+			member_data.ellipsoid_code IS NOT NULL
+		) AND
+		(
+			d.prime_meridian_code IS NOT NULL OR
+			member_data.prime_meridian_code IS NOT NULL
+		)
+`, (result) => {
+	const id = result.id + "";
+
+	if (addedIds[id])
+		return;
+
+	addedIds[id] = true;
+	const meridian = (result.pm || result.ref_pm) + "";
+	const ellipsoid = (result.ellipsoid || result.ref_ellipsoid) + "";
 	let str = "";
+
 	if (ellipsoid in ellipsoids)
 		str += ellipsoids[ellipsoid];
+
 	if (meridian in meridians)
 		str += " +pm=" + meridians[meridian];
 
-	let index = result.id.toString();
-	if (KnownDatums[index])
-		str += " +towgs84=" + KnownDatums[index];
+	if (KnownDatums[id])
+		str += " +towgs84=" + KnownDatums[id];
 
 	return str;
 }, "GeogGeodeticDatumGeoKey", `/**
