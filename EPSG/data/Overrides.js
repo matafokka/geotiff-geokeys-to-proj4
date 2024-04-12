7226410587e1b6636e84194220bcf75199179822
matafokka <da.real.matafokka@gmail.com>
2024-04-12 20:21:04 +0300

    Added +lat_0 > +lat_ts override for Mercator

--- EPSG/data/Overrides.js
+++ EPSG/data/Overrides.js
@@ -6,10 +6,15 @@ module.exports = function override(tokens) {
 	let proj = tokens["+proj"], a = tokens["+a"], b = tokens["+b"];
 
 	// Some geokeys should be mapped to different Proj4 parameters than specified in PCSKeys.js
-	// For now, only this transform is known. Others might come up later.
+
 	if (proj === "cea") {
 		tokens["+lat_ts"] = tokens["+lat_1"];
 		delete tokens["+lat_1"];
+	}
+
+	if (proj === "merc") {
+		tokens["+lat_ts"] = tokens["+lat_0"];
+		delete tokens["+lat_0"];
 	}
 
 	// These projections doesn't work with spheres, proj4 requires +approx parameter in this case
